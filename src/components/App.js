e834a21b6a5ecc475f5c4648b4536bfd11611532
Jaime Banda <jbanda@aiig.local>
2018-08-29 17:04:36 -0400

    Change component App from presentational to componentc

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,31 @@
+import React, { Component, PropTypes } from 'react';
 import Search from './ui/Search'
 import AlbumList from './ui/AlbumList'
 
-const App = () =>
-    <div className="app">
-        <Search />
-        <AlbumList />
-    </div>
+class App extends Component {
+    
+    componentWillMount() {
+        console.log("componentWillMount")
+    }
+
+    render() {
+        console.log('props: ');
+        console.log(this.props);
+
+        return (
+            <div>
+                <Search />
+                <AlbumList />
+            </div>
+            // <ul>
+            //     {this.props.items.map((item) => (
+            //         <li key={item.id}>
+            //             {item.label}
+            //         </li>
+            //     ))}
+            // </ul>
+        );
+    }
+}
 
 export default App
