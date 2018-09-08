06fa24daf877f1dad224ae9c072cae0eb8a10f5d
Alonso Banda <jaime.banda@outlook.com>
2018-09-07 23:03:44 -0400

    remove comments

--- src/src/store/index.js
+++ src/src/store/index.js
@@ -4,12 +4,6 @@ import { filterBy, data, hasErrored, isLoading } from './Reducers'
 
 const stateData = {}
 
-// const storeCreated = createStore(
-//     combineReducers({ hasErrored, isLoading, filterBy, data }),
-//     stateData,
-//     applyMiddleware(thunk)
-// )
-
 const storeFactory = (initialState = stateData) =>
     applyMiddleware(thunk)(createStore)(
         combineReducers({ hasErrored, isLoading, filterBy, data }),
