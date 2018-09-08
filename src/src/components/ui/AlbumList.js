5b5ab31c71b6d44c0a0ce13377469ea957c80ead
Alonso Banda <jaime.banda@outlook.com>
2018-09-07 23:06:01 -0400

    remove comments

--- src/src/components/ui/AlbumList.js
+++ src/src/components/ui/AlbumList.js
@@ -1,28 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import AlbumItem from './AlbumItem'
 
-// const AlbumList = ({ feed = {}, filterBy = "", hasErrors = false, isLoading = false, fetchData = f=>f }) =>
-//     <div className="container">
-//         {
-//             hasErrors ?
-//                 <p>Errors, unable to pull the data</p> :
-//                 isLoading ?
-//                     <p>Loading Data</p> :                    
-//                     <div className="row text-center list-group">
-//                         {feed.entry !== undefined && feed.entry.length === 0 ?
-//                             <p>No Albums to display.</p> :
-//                             filterBy ?
-//                                 feed.entry.filter(e => e['im:artist'].label.indexOf(filterBy) > -1).map((e, i) => <AlbumItem key={i} {...e} />) :
-//                                 feed.entry.map((e, i) => <AlbumItem key={i} {...e} />)
-//                         }
-//                     </div>                
-//         }
-//     </div>
-// AlbumList.componentDidMount = () => {
-//     console.log("componentDidMount event in AlbumList Componenet")
-//     fetchData('https://itunes.apple.com/us/rss/topalbums/limit=100/json');
-// };
-
 class AlbumList extends Component {
     componentDidMount() {
         console.log("componentDidMount event in AlbumList Component")
